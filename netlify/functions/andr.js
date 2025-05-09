ff2e3fefd04754071edb29b3a851af79b442430d
ggmygf <jinghunnai@gmail.com>
2025-05-09 11:53:22 -0700

    Update andr.js

--- netlify/functions/andr.js
+++ netlify/functions/andr.js
@@ -5,10 +5,11 @@ const pool = new Pool({
 });
 
 exports.handler = async (event, context) => {
-  const code = event.queryStringParameters.code;
+//  const code = event.queryStringParameters.code;
   let keyy; // Declare keyy outside the try block
   let client; // Declare client outside the try block
-
+  
+/*
   try {
     client = await pool.connect();
     const result = await client.query('SELECT note FROM one_time_keys WHERE auth_key = $1', [code]);
@@ -35,7 +36,7 @@ exports.handler = async (event, context) => {
       client.release(); // Release the client in the finally block
     }
   }
-
+*/
   const configText = `port: 7890
 socks-port: 7891
 allow-lan: true
@@ -53,7 +54,7 @@ dns:
     - tls://1.0.0.1:853
     - tls://dns.google:853
 proxies:
-  - {"name": "1-paid", "server": "143.198.228.81", "port": 3655, "client-fingerprint": "chrome", "type": "vless", "uuid": "139d5d44-472c-4141-88d0-57cbb82393c8", "tls": true, "alpn": ["h3", "h2", "http/1.1"], "tfo": false, "skip-cert-verify": false, "network": "tcp"}`;
+  - {"name": "1-paid", "server": "143.198.228.81", "port": 3655, "client-fingerprint": "chrome", "type": "vmess", "uuid": "139d5d44-472c-4141-88d0-57cbb82393c8", "tls": true, "alpn": ["h3", "h2", "http/1.1"], "tfo": false, "skip-cert-verify": false, "network": "tcp"}`;
 
   return {
     statusCode: 200,
