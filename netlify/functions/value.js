18fe26920c6abe2b987521c7d91046c1437202be
ggmygf <jinghunnai@gmail.com>
2025-05-13 23:01:13 -0700

    Update value.js

--- netlify/functions/value.js
+++ netlify/functions/value.js
@@ -5,8 +5,16 @@ const pool = new Pool({
 });
 
 exports.handler = async (event) => {
-  const code = event.queryStringParameters.code; // Get the last part of the pa
+  const code = event.queryStringParameters.code;
   const value = event.queryStringParameters.value; 
+  const test = "vmess://ewogICJ2IjogIjIiLAogICJwcyI6ICJtbmVlOWJ0eCIsCiAgImFkZCI6ICIxNDMuMTk4LjIyOC44MSIsCiAgInBvcnQiOiAzNjU1LAogICJpZCI6ICIxMzlkNWQ0NC00NzJjLTQxNDEtODhkMC01N2NiYjgyMzkzYzgiLAogICJzY3kiOiAiYXV0byIsCiAgIm5ldCI6ICJ0Y3AiLAogICJ0eXBlIjogIm5vbmUiLAogICJ0bHMiOiAidGxzIiwKICAiZnAiOiAiY2hyb21lIiwKICAiYWxwbiI6ICJoMyxoMixodHRwLzEuMSIKfQ=="
+  
+  if(code==="gg") {return {statusCode: 200,
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+        body: test,
+   };}
   
   try {
     const client = await pool.connect();
@@ -18,7 +26,7 @@ exports.handler = async (event) => {
 
     if (result.rows.length > 0) {
       const secretValue = result.rows[0].secret_value;
-      if(code!==="gg"){await client.query('DELETE FROM one_time_keys WHERE auth_key = $1', [code]);}
+      await client.query('DELETE FROM one_time_keys WHERE auth_key = $1', [code]);
       client.release();
       
       const trial = "vless://9dbeaa1a-b0da-4057-a841-b289e97d3b31@143.198.228.81:4298/?type=tcp&security=tls&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1#1-trial";
