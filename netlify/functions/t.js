2fb25ad3475353611010ace669af5a3b6ee3f6a1
ggmygf <jinghunnai@gmail.com>
2025-05-15 01:34:09 -0700

    Update t.js

--- netlify/functions/t.js
+++ netlify/functions/t.js
@@ -6,7 +6,7 @@ exports.handler = async function(event, context) {
     headers: {
       "Content-Type": "text/plain"
     },
-    body: "vless://YXV0bzpkZTJkNTM1My1mY2QzLTQxYzAtYTkwOS1mZWU2ZTdmYWNjNmRAMTQzLjE5OC4yMjguODE6NDI5OA?remarks=1-paid&obfs=none&tls=1&alpn=h3,h2,http/1.1"
-    // body: "vless://9dbeaa1a-b0da-4057-a841-b289e97d3b31@143.198.228.81:4298/?type=tcp&security=tls&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1#1-trial"
+    body: "vless://de2d5353-fcd3-41c0-a909-fee6e7facc6d@143.198.228.81:4298?remarks=1-paid&obfs=none&tls=1&alpn=h3,h2,http/1.1"
+    //body: "vless://9dbeaa1a-b0da-4057-a841-b289e97d3b31@143.198.228.81:4298/?type=tcp&security=tls&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1#1-trial"
   };
 };
