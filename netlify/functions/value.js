005c79f4708739159bb6ae180e27a559a2c9c416
ggmygf <jinghunnai@gmail.com>
2025-05-17 04:56:55 -0700

    Update value.js

--- netlify/functions/value.js
+++ netlify/functions/value.js
@@ -32,8 +32,8 @@ exports.handler = async (event) => {
       const trial = "vless://9dbeaa1a-b0da-4057-a841-b289e97d3b31@143.198.228.81:4298/?type=tcp&security=tls&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1#1-trial";
       const paid = "vless://de2d5353-fcd3-41c0-a909-fee6e7facc6d@143.198.228.81:4298/?type=tcp&security=tls&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1#1-paid";
       let final = "get lost";
-      if (secretValue==="paid"){final = paid;}
-      if (secretValue==="trial"){final = trial;} 
+      if (secretValue==='paid'){final = paid;}
+      if (secretValue==='trial'){final = trial;} 
         
       return {
         statusCode: 200,
